e1d5440eb647d8ad0b1420a05a260cbe16b71814
mrnoob-coding <54588215+mrnoob-coding@users.noreply.github.com>
2022-03-08 22:42:21 +0530

    s

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce/homepage.dart';
 import 'package:ecommerce/splash.dart';
 import 'package:flutter/material.dart';
 
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: const Splash(),
+      home: Homepage(),
     );
   }
 }

21caa5abb13924a1f22879e3276478063bd128a1
mrnoob-coding <54588215+mrnoob-coding@users.noreply.github.com>
2022-03-09 02:59:20 +0530

    row sorted

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:carousel_slider/carousel_slider.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({ Key? key }) : super(key: key);
@@ -9,6 +9,10 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+ final imagepaths = [
+   'images/ui elements/homepage/banner_1.png',
+    'images/ui elements/homepage/banner_2.png'
+ ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,6 +50,7 @@ class _HomepageState extends State<Homepage> {
               mainAxisSize: MainAxisSize.max,
               children: [
                 Container(
+                  height: 20.0,
                   width: MediaQuery.of(context).size.width*3/4,
                   color: Color(0xffFFAC00),
                   child: Row(
@@ -61,13 +66,57 @@ class _HomepageState extends State<Homepage> {
                   ),
                 ),
                 Container(
+                  height: 20.0,
                   width: MediaQuery.of(context).size.width*1/4,
                   color: Color(0xffFCC129),
                   child: Text('ROYALS OF ROYALOOO',style: TextStyle(fontSize: 10),),
                 )
-                
               ],
             ),
+            Container(
+              height: MediaQuery.of(context).size.height*1/3.5,
+              width: MediaQuery.of(context).size.width,
+              color: Color(0xffFCC120).withOpacity(0.5),
+              child: CarouselSlider(options: CarouselOptions(autoPlay: true,
+              autoPlayInterval: Duration(seconds: 3),
+              autoPlayAnimationDuration: Duration(milliseconds: 800),
+              autoPlayCurve: Curves.fastOutSlowIn,), items: imagepaths.map((imagepath) {
+                return Image.asset(imagepath,fit: BoxFit.fill);
+              }).toList(),)
+            ),
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      Text('FLAT 10% OFF',style: TextStyle(fontSize: 20),),
+                      Text('on all sale',style: TextStyle(fontSize: 10),),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Text('UNDER 3000 ',style: TextStyle(fontSize: 20),),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Text('STORE',style: TextStyle(fontSize: 20),),
+                          SizedBox(width: 10.0,),
+                      Text('Hot Deals',style: TextStyle(fontSize: 10),),
+                        ],
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Text('FROM BANARAS',style: TextStyle(fontSize: 20),),
+                      Text('top products',style: TextStyle(fontSize: 10),),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            
           ],
         ),
       ),),
